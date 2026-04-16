42d3e3b37da302751501407cd6a867f9965b26c0
root <root@HOSTCOIN-2853-4220.com>
2026-04-16 12:42:03 +0530

    First commit on new server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-    allowedHosts: ['lavanistore.in', 'www.lavanistore.in']
+    allowedHosts: ['all']
   },
   base: "/"
 })
